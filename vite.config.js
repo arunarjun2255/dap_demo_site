11ec650dd37d391f8dc14f40ca87d72cb948b3fb
arunarjun2255 <140376358+arunarjun2255@users.noreply.github.com>
2026-07-01 22:18:46 +0530

    new changes 004

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/dap_demo_site",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cogniorbackend-v1.azurewebsites.net',
+        changeOrigin: true,
+      },
+    },
+  },
 });
